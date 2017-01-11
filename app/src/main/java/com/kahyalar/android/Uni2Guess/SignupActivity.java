78560c18d8c652d8dc424347d2da57bac3020e60
kahyalar <furkan.kahyalar@gmail.com>
2017-01-11 22:34:24 +0300

    Signup activity xml finished

---
+++ app/src/main/java/com/kahyalar/android/Uni2Guess/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.kahyalar.android.Uni2Guess;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kahyalar on 11/01/2017.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
