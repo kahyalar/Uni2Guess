87033628faec24fb8f922158df2aa916092826cb
kahyalar <furkan.kahyalar@gmail.com>
2017-01-12 13:38:58 +0300

    init elements for signup activity

--- app/src/main/java/com/kahyalar/android/Uni2Guess/SignupActivity.java
+++ app/src/main/java/com/kahyalar/android/Uni2Guess/SignupActivity.java
@@ -2,15 +2,30 @@ package com.kahyalar.android.Uni2Guess;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 /**
  * Created by kahyalar on 11/01/2017.
  */
 
 public class SignupActivity extends AppCompatActivity {
+    private EditText txtName, txtSurname, txtBirthDate, txtUniversity, txtDepartment, txtGrade, txtCurrentGPA, txtStudentMail;
+
+    private void initElements(){
+        txtName = (EditText)findViewById(R.id.txtName);
+        txtSurname = (EditText)findViewById(R.id.txtSurname);
+        txtBirthDate = (EditText)findViewById(R.id.txtBirthDate);
+        txtUniversity = (EditText)findViewById(R.id.txtUniversity);
+        txtDepartment = (EditText)findViewById(R.id.txtDepartment);
+        txtGrade = (EditText)findViewById(R.id.txtGrade);
+        txtCurrentGPA = (EditText)findViewById(R.id.txtCurrentGPA);
+        txtStudentMail = (EditText)findViewById(R.id.txtStudentMail);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        initElements();
     }
 }
