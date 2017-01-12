fd89135c83efe147f535fd87575fbd92dfdfd640
kahyalar <furkan.kahyalar@gmail.com>
2017-01-12 13:53:21 +0300

    init events func and btnclick event added for signup activity

--- app/src/main/java/com/kahyalar/android/Uni2Guess/SignupActivity.java
+++ app/src/main/java/com/kahyalar/android/Uni2Guess/SignupActivity.java
@@ -2,7 +2,10 @@ package com.kahyalar.android.Uni2Guess;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by kahyalar on 11/01/2017.
@@ -10,6 +13,7 @@ import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
     private EditText txtName, txtSurname, txtBirthDate, txtUniversity, txtDepartment, txtGrade, txtCurrentGPA, txtStudentMail;
+    private Button btnCreateNewUser;
 
     private void initElements(){
         txtName = (EditText)findViewById(R.id.txtName);
@@ -20,12 +24,30 @@ public class SignupActivity extends AppCompatActivity {
         txtGrade = (EditText)findViewById(R.id.txtGrade);
         txtCurrentGPA = (EditText)findViewById(R.id.txtCurrentGPA);
         txtStudentMail = (EditText)findViewById(R.id.txtStudentMail);
+        btnCreateNewUser = (Button)findViewById(R.id.btnCreateNewUser);
     }
 
+    private void initEvents(){
+        btnCreateNewUser_Click();
+    }
+
+    public void btnCreateNewUser_Click(){
+        btnCreateNewUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String yazilacakText = String.format("Adınız: %s\nSoyadınız: %s\nDoğum Gününüz: %s\n" +
+                        "Üniversiteniz: %s\nBölümünüz: %s\nSınıfınız: %s\nŞuanki Not Ortalamanız: %s\n" +
+                        "Öğrenci Mailiniz: %s\n",txtName.getText(),txtStudentMail.getText(),txtBirthDate.getText(),txtUniversity.getText(),
+                        txtDepartment.getText(),txtGrade.getText(),txtCurrentGPA.getText(),txtStudentMail.getText());
+                Toast.makeText(SignupActivity.this, yazilacakText, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         initElements();
+        initEvents();
     }
 }
